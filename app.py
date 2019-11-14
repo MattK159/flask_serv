cf5cd37fa1f12ffd5dd8efc9225445276e32e1bf
MattK159 <matthew.keating2@mycit.ie>
2019-11-14 21:07:20 +0000

    update

--- app.py
+++ app.py
@@ -80,5 +80,5 @@ def load_model(model):
 # start the flask app, allow remote connections
 if __name__ == '__main__':
    port = int(os.environ.get("PORT", 5000))
-   app.run(debug=True, port=port)
+   app.run(port=port)
 
