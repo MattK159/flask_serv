ebf8bfa38f418f3804b98a4cc938324c9e8a2a5d
MattK159 <matthew.keating2@mycit.ie>
2020-01-06 15:17:05 +0000

    fixed heart test

--- localtest.py
+++ localtest.py
@@ -2,7 +2,7 @@ import requests
 
 diabvals = {'model': 1, 'np': 6, "pgc": 148, "dpb": 72, "tst": 35, "si": 0, "bmi": 33.6, "dpf": 0.627, "age": 50}
 canvals = {'model': 2, 'age': 48, "bmi": 23.5, "gluc": 70, "inc": 2.707, "HOMA": 0.467408667, "lep": 8.8071, "adi": 9.7024, "res": 7.99585, "mcp": 417.114}
-heartvals = {'model': 3, 'age': 48, "bmi": 23.5, "gluc": 70, "inc": 2.707, "HOMA": 0.467408667, "lep": 8.8071, "adi": 9.7024, "res": 7.99585, "mcp": 11.114, "aa": 16.114, "ab": 10.114, "ac": 17.114, "ad": 12}
+heartvals = {'model': 3, 'age': 48, "bmi": 18.5, "gluc": 45, "inc": 2.707, "HOMA": 0.467408667, "lep": 8.8071, "adi": 9.7024, "res": 7.99585, "mcp": 11.114, "aa": 16.114, "ab": 10.114, "ac": 17.114, "ad": 12}
 
 result = requests.get("http://0.0.0.0:5000", params = diabvals)
 print(result.json())
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import requests
 
 diabvals = {'model': 1, 'np': 6, "pgc": 148, "dpb": 72, "tst": 35, "si": 0, "bmi": 33.6, "dpf": 0.627, "age": 50}
 canvals = {'model': 2, 'age': 48, "bmi": 23.5, "gluc": 70, "inc": 2.707, "HOMA": 0.467408667, "lep": 8.8071, "adi": 9.7024, "res": 7.99585, "mcp": 417.114}
-heartvals = {'model': 3, 'age': 48, "bmi": 23.5, "gluc": 70, "inc": 2.707, "HOMA": 0.467408667, "lep": 8.8071, "adi": 9.7024, "res": 7.99585, "mcp": 11.114, "aa": 16.114, "ab": 10.114, "ac": 17.114, "ad": 12}
+heartvals = {'model': 3, 'age': 48, "bmi": 1, "gluc": 1, "inc": 145, "HOMA": 233, "lep": 1, "adi": 2, "res": 150, "mcp": 0, "aa": 2.3, "ab": 3, "ac": 0, "ad": 6}
 
 
 
